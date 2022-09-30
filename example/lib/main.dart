1d7f0836612f9da32e265a50942bbf6edf3cb2ba
Ridwan Fauzan <rymetutor@gmail.com>
2022-09-30 16:31:11 +0700

    update dependencies + fix deprecated icon

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:thumbnailer/thumbnailer.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:thumbnailer/thumbnailer.dart';
 
 void main() {
   runApp(MyApp());
